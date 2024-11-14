835750733809dc1dfc8ef791ab6145137a3999c0
susmitha sundar <susmitha.sundar@syncfusion.com>
2024-11-14 16:02:40 +0530

    Sample committed

---
+++ SemanticKernalAI.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SmartFilterPredicates_Demo
+{
+    public class SemanticKernelAI
+    {
+        /// <summary>
+        /// Holds the IChatCompletionService instance.
+        /// </summary>
+        private IChatCompletionService chatCompletionService;
+
+        /// <summary>
+        /// Holds the Kernel instance.
+        /// </summary>
+        private Kernel kernel;
+
+        /// <summary>
+        /// Method to get the response from OpenAI using the semantic kernel
+        /// </summary>
+        /// <param name="prompt">Prompt for the system message</param>
+        /// <returns>The AI results.</returns>
+        public async Task<string> GetResponseFromGPT(string prompt)
+        {
+            try
+            {
+                // Create a kernel with Azure OpenAI chat completion
+                var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion("AI-Integration", "https://wpf-datagrid-desktop.openai.azure.com/", "04a0d89428024c1f864e1efc294aa55e");
+
+                // Build the kernel
+                kernel = builder.Build();
+
+                chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+                var history = new ChatHistory();
+                history.AddSystemMessage(prompt);
+
+                // Set the execution settings
+                OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new OpenAIPromptExecutionSettings();
+                openAIPromptExecutionSettings.ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions;
+
+                // Get the response from the AI 
+                var result = await chatCompletionService.GetChatMessageContentAsync(
+                history,
+                executionSettings: openAIPromptExecutionSettings,
+                kernel: kernel);
+
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                // Return an empty string if an exception occurs
+                return string.Empty;
+            }
+        }
+
+       
+    }
+}
